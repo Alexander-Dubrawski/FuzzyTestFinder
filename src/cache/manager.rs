6022a525dc01d04e7292d9f22ac3a26e2cbd2e30
Alexander Dubrawski <alexander.dubrawski@traderepublic.com>
2025-04-13 16:50:48 +0200

    Improve caching by checking for new tests if file modified

--- src/cache/manager.rs
+++ src/cache/manager.rs
@@ -1,29 +1,80 @@
 use home::home_dir;
+use rustpython_parser::{Mode, lexer::lex, parse_tokens, source_code};
 use walkdir::WalkDir;
 
 use super::types::CacheEntry;
 use std::{
+    collections::{HashMap, HashSet},
+    ffi::OsStr,
     fs::File,
     io::{BufReader, BufWriter},
     path::{Path, PathBuf},
     time::{SystemTime, UNIX_EPOCH},
 };
 
-fn check_change(root: &str, since: u128) -> bool {
+fn collect_tests_from_file(path: &Path) -> HashSet<String> {
+    let source_code = std::fs::read_to_string(path).unwrap();
+    let tokens = lex(source_code.as_str(), Mode::Module);
+    let ast = parse_tokens(tokens, Mode::Module, "<embedded>").unwrap();
+    let mut tests = HashSet::new();
+    match ast {
+        rustpython_parser::ast::Mod::Module(mod_module) => {
+            for stmt in mod_module.body.iter() {
+                match stmt {
+                    rustpython_parser::ast::Stmt::FunctionDef(stmt_function_def) => {
+                        let test_name = stmt_function_def.name.to_string();
+                        if test_name.starts_with("test") {
+                            tests.insert(stmt_function_def.name.to_string());
+                        }
+                    }
+                    _ => continue,
+                }
+            }
+        }
+        _ => todo!(),
+    }
+    tests
+}
+
+fn check_file_for_new_tests(path: &Path, tests: &HashSet<String>) -> bool {
+    let collected_tests = collect_tests_from_file(path);
+    &collected_tests != tests
+}
+
+fn check_change(root: &str, since: u128, tests: &HashMap<String, HashSet<String>>) -> bool {
     for entry in WalkDir::new(root) {
         let entry = entry.unwrap();
         if entry.file_type().is_file() {
             let metadata = std::fs::metadata(entry.path()).unwrap();
+            if entry.path().extension().is_none() {
+                continue;
+            }
+
+            if entry.path().extension().and_then(OsStr::to_str).unwrap() != "py" {
+                continue;
+            }
+
+            // TODO: Check if modified, that if modified, that there are new/removed functions
+            // If so Cache entry can be replaced in place
             if let Ok(modified) = metadata.modified() {
                 if modified.duration_since(UNIX_EPOCH).unwrap().as_millis() > since {
                     println!("Modified: {:?}", entry.path());
-                    return true; // Found a modified file
+                    let full_path = entry.path().as_os_str().to_str().unwrap();
+                    let relative_path = format!("tests{}", full_path.strip_prefix(root).unwrap());
+                    if check_file_for_new_tests(entry.path(), &tests[&relative_path]) {
+                        println!("New tests found");
+                        return true;
+                    }
                 }
             }
+            // TODO: Check if created, that the file includes test function
             if let Ok(created) = metadata.created() {
                 if created.duration_since(UNIX_EPOCH).unwrap().as_millis() > since {
                     println!("New file: {:?}", entry.path());
-                    return true;
+                    if !collect_tests_from_file(entry.path()).is_empty() {
+                        println!("New tests found");
+                        return true;
+                    }
                 }
             }
         }
@@ -42,7 +93,11 @@ pub fn get_entry(project_id: &str) -> Option<CacheEntry> {
     let entry = File::open(file_path).unwrap();
     let reader = BufReader::new(entry);
     let cache_entry: CacheEntry = serde_json::from_reader(reader).unwrap();
-    if check_change(cache_entry.root_folder.as_str(), cache_entry.timestamp) {
+    if check_change(
+        cache_entry.root_folder.as_str(),
+        cache_entry.timestamp,
+        &cache_entry.tests,
+    ) {
         return None;
     }
     println!("Cache Hit.");
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use FzT::python::pytest::run;
 // Add support for --query and direct it to fzf
 // Add cache function / one cache per project
 // Add window preview mode seeing the code
+// Add cache clear option
 fn main() {
     run();
 }
