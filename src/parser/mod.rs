ef33fe05e5d07d0d648705a8e737f5900d5dec30
Alexander Dubrawski <alexander.dubrawski@traderepublic.com>
2025-04-20 08:38:47 +0200

    Define traits

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,17 @@
 use crate::cache::types::CacheEntry;
+mod python;
 
 pub trait Parser {
     fn parse_test(&self) -> CacheEntry;
     fn update_tests(&self, cache_entry: &mut CacheEntry) -> bool;
+}
+
+pub trait Test {
+    fn runtime_argument(self) -> String;
+    fn name(&self) -> String;
+}
+
+pub trait Tests {
+    fn to_json(&self) -> String;
+    fn tests(self) -> Vec<impl Test>;
 }
