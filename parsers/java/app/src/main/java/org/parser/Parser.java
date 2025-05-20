7c70a610a4a568032959e98dd05ff7286ae74069
Alexander Dubrawski <alexander.dubrawski@traderepublic.com>
2025-05-20 11:19:06 +0200

    Use relative path for java tests

--- parsers/java/app/src/main/java/org/parser/Parser.java
+++ parsers/java/app/src/main/java/org/parser/Parser.java
@@ -10,7 +10,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import java.util.logging.Logger;
+
 public class Parser {
+    private static final Logger logger = Logger.getLogger(Parser.class.getName());
 
     public String parse(String projectPath, String cacheJson) throws IOException {
         JavaTests javaTests;
@@ -23,6 +26,10 @@ public class Parser {
             final ObjectMapper objectMapper = new ObjectMapper();
             javaTests = objectMapper.readValue(cacheJson, new TypeReference<>() {
             });
+            if (javaTests.rootFolder != projectPath) {
+                logger.warning("Root Folders are different. Cache: " + javaTests.rootFolder + " != Project path : " + projectPath + "Root folder set to project folder");
+                javaTests.rootFolder = projectPath;
+            }
         }
         javaTests.update();
         final ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
