a9e8f85991bca9f5bafc1aff2ce0f21c9f24667b
Alexander Dubrawski <alexander.dubrawski@traderepublic.com>
2025-09-21 20:12:20 +0200

    Multi threaded cargo test runtime

--- src/runtime/utils.rs
+++ src/runtime/utils.rs
@@ -29,3 +29,35 @@ pub fn run_and_capture_print<F: RuntimeFormatter>(
     let plain_bytes = strip_ansi_escapes::strip(output.as_bytes());
     String::from_utf8(plain_bytes).map_err(|e| FztError::from(e))
 }
+
+pub fn partition_tests(vec: &[String], m: usize) -> Vec<Vec<String>> {
+    let n = vec.len();
+    if m == 0 {
+        return Vec::new();
+    }
+    if n == 0 {
+        return vec![];
+    }
+    let mut partitions = Vec::new();
+
+    if n < m {
+        // Each element gets its own partition
+        for item in vec {
+            partitions.push(vec![item.clone()]);
+        }
+    } else {
+        let base_size = n / m;
+        let remainder = n % m;
+        let mut start = 0;
+
+        for i in 0..m {
+            let mut end = start + base_size;
+            if i < remainder {
+                end += 1;
+            }
+            partitions.push(vec[start..end].to_vec());
+            start = end;
+        }
+    }
+    partitions
+}
