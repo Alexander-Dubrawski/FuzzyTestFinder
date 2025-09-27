d42cf366bfc3328d95971864f4520bd3785afe95
Alexander Dubrawski <alexander.dubrawski@traderepublic.com>
2025-09-27 23:00:20 +0200

    Define library api

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,52 @@
 pub mod cache;
 pub mod cli;
 pub mod errors;
-pub mod runner;
-pub mod runtime;
-pub mod search_engine;
-pub mod tests;
-pub mod utils;
+mod runner;
+mod runtime;
+mod search_engine;
+mod tests;
+mod utils;
+
+pub use cache::Cache;
+pub use cache::manager::LocalCacheManager;
+pub use errors::FztError;
+
+pub use runner::Runner;
+pub use runner::config::FilterMode;
+pub use runner::config::Language;
+pub use runner::config::Preview;
+pub use runner::config::RunnerConfig;
+pub use runner::config::RunnerMode;
+pub use runner::java::get_java_runner;
+pub use runner::python::get_python_runner;
+pub use runner::rust::get_rust_runner;
+
+pub use runtime::Debugger;
+pub use runtime::JavaDebugger;
+pub use runtime::PythonDebugger;
+pub use runtime::Runtime;
+pub use runtime::RustDebugger;
+pub use runtime::java::gradle::GradleRuntime;
+pub use runtime::python::pytest::PytestRuntime;
+pub use runtime::rust::cargo::CargoRuntime;
+
+pub use search_engine::SearchEngine;
+pub use search_engine::fzf::FzfSearchEngine;
+
+pub use tests::Test;
+pub use tests::Tests;
+pub use tests::java::java_test::JavaTestItem;
+pub use tests::java::java_test::JavaTests;
+pub use tests::java::parser::JavaParser;
+pub use tests::python::pytest::tests::PytestTests;
+pub use tests::python::python_test::PythonTest;
+pub use tests::python::rust_python::tests::RustPytonTests;
+pub use tests::rust::rust_test::RustTest;
+pub use tests::rust::rust_test::RustTestItem;
+pub use tests::rust::rust_test::RustTests;
+pub use tests::test_provider::SelectGranularity;
+pub use tests::test_provider::TestProvider;
+
+pub use utils::file_walking::collect_tests;
+pub use utils::file_walking::filter_out_deleted_files;
+pub use utils::path_resolver::get_relative_path;
